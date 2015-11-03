a920fd8dd25521f127c3f9c0cccc08b7bb225d82
TMC-Dev1 <sudhakar.gaddam@groupfio.com>
2015-11-03 09:10:05 +0800

    Changed the class name for PersonalInfo

--- rodin.core/src/main/java/com/bizify/rodin/core/domain/PersonInfoContactMechPurpose.java
+++ rodin.core/src/main/java/com/bizify/rodin/core/domain/PersonInfoContactMechPurpose.java
@@ -12,14 +12,14 @@ import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name="PersonInfoContactMechPurpose", uniqueConstraints = {
-                                                                 @UniqueConstraint(columnNames = "persanalInfo.id"), 
+                                                                 @UniqueConstraint(columnNames = "PersonalInformation.id"), 
                                                                  @UniqueConstraint(columnNames = "contactMech.id"), 
                                                                  @UniqueConstraint(columnNames = "contactMechPurposeType.id")})
 public class PersonInfoContactMechPurpose extends BaseDomain {
    
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "persanalInfo.id", insertable = false, updatable = false)
-    private PersonalInfo personalInfo = null;
+    @JoinColumn(name = "personalInformation.id", insertable = false, updatable = false)
+    private PersonalInformation personalInformation = null;
     
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "contactMech.id", insertable = false, updatable = false)

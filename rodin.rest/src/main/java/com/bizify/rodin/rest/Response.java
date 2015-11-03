f8b90f430ff8dc141d77ace28098245c444cc1c2
vener.guevarra@gmail.com <vener.guevarra@gmail.com>
2015-11-04 07:59:32 +0800

    Added response builder

--- rodin.rest/src/main/java/com/bizify/rodin/rest/Response.java
+++ rodin.rest/src/main/java/com/bizify/rodin/rest/Response.java
@@ -2,6 +2,7 @@ package com.bizify.rodin.rest;
 
 import java.io.Serializable;
 
+import com.bizify.rodin.rest.interfaces.Result;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
@@ -37,5 +38,30 @@ public class Response<T extends Serializable> extends BaseResult<T> implements S
         public String toString() {
             return this.value;
         }
+    }
+
+    public static <T extends Serializable> Builder<T> builder() {
+        return new Builder<T>();
+    }
+
+    @SuppressWarnings({ "rawtypes" })
+    public static class Builder<T extends Serializable> {
+        private Result result;
+
+        public Builder withResult(Result result) {
+            this.result = result;
+            return this;
+        }
+
+        public Response<Serializable> build() {
+            Response<Serializable> response = new Response<Serializable>();
+            if (result != null) {
+                response.setData(result.getData());
+                response.setMessages(result.getMessages());
+            }
+
+            return response;
+        }
+
     }
 }
